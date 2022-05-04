0b93cc8a384e280ae7ec6fbfdf236755d9265783
kilian <kilian.hammer@gmx.de>
2022-05-04 13:02:33 +0200

    added web application

---
+++ web_application/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname + '/static/index.html'))
+})
+
+app.listen(port, () => {
+  console.log('Server listening on', port)
+})
+
