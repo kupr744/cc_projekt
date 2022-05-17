998470e44ca83db3561ba8f995fa982d63b915e5
Your Name <you@example.com>
2022-05-17 21:41:15 +0200

    Add Dockerfiles, .yaml Files for Cluster; Upsate backend/index.js

--- web_application/backend/index.js
+++ web_application/backend/index.js
@@ -8,6 +8,28 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname.split('backend')[0] + 'static/index.html'))
 })
 
+
+//Sollte einen request senden an den Service der die Primzahlen berechnet 
+app.get('/prime/:to/:from', async(req,res) => {
+  if(req.params.to === undefined && req.params.from === undefined) {
+    res.status(400).send("Syntax Error")
+  } else if(req.params.from === undefined) {
+    res.setHeader('Content-Type', 'application/json');
+    var primes = prime(1, parseInt(req.params.to))
+    res.status(200).send({"result": primes})
+
+  } else {
+    response = await fetch(`http://primeservice/prime/${req.params.to}/${req.params.from}`)
+    const primes = await respose.json();
+
+    return res.json({
+      primes,
+    });
+  }
+ 
+
+})
+
 app.listen(port, () => {
   console.log('Server listening on', port)
 })
