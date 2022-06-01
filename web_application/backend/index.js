17865425307d9df6259e0bc7eb95b5b10ba24f10
Jens Dostal <92doje1bif@hft-stuttgart.de>
2022-06-01 17:17:06 +0200

    Fix: Kubernetes Services and Deployments to enter locally

--- web_application/backend/index.js
+++ web_application/backend/index.js
@@ -2,32 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const port = 3000
+require('dotenv').config();
+
+const port = process.env.NODE_ENV === 'development' ? 3000 : null;
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname.split('backend')[0] + 'static/index.html'))
-})
-
-
-//Sollte einen request senden an den Service der die Primzahlen berechnet 
-app.get('/prime/:to/:from', async(req,res) => {
-  if(req.params.to === undefined && req.params.from === undefined) {
-    res.status(400).send("Syntax Error")
-  } else if(req.params.from === undefined) {
-    res.setHeader('Content-Type', 'application/json');
-    var primes = prime(1, parseInt(req.params.to))
-    res.status(200).send({"result": primes})
-
-  } else {
-    response = await fetch(`http://primeservice/prime/${req.params.to}/${req.params.from}`)
-    const primes = await respose.json();
-
-    return res.json({
-      primes,
-    });
-  }
- 
-
+	res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(port, () => {
