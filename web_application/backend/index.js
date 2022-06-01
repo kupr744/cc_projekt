51809433aa39f48bbc2ff5cbb6459a93fe2ad0a4
kilian <kilian.hammer@gmx.de>
2022-06-01 18:29:06 +0200

    added test route

--- web_application/backend/index.js
+++ web_application/backend/index.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const axios = require('axios')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json());
 require('dotenv').config();
 
 const port = process.env.NODE_ENV === 'development' ? 3000 : null;
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/test', async (req, res) => {
+  await axios('http://primeservice-clusterip-service/prime/10/1', 
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+        }
+        })
+        .then(data => res.send(data))
+        .catch(err => res.send(err))  
+  res.status(200).send('test')
 })
 
 app.listen(port, () => {
