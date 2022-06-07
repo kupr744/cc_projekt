81a1c3959eafc5e1372c0a06c8f9b6bbca4c58b0
kilian <kilian.hammer@gmx.de>
2022-06-07 21:40:21 +0200

    remove test route

--- web_application/backend/index.js
+++ web_application/backend/index.js
@@ -18,18 +18,6 @@ app.get('/', (req, res) => {
 	res.status(200).sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.get('/test', async (req, res) => {
-  await axios('http://primeservice-clusterip-service/prime/10/1', 
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-        }
-        })
-        .then(data => res.status(200).send({"result": data.data.result, "host": process.env.HOSTNAME}))
-        .catch(err => res.send(err))  
-})
-
 app.listen(port, () => {
   console.log("node_env:", process.env.NODE_ENV)
   console.log('Server listening on', port)
